a6aeb7dd73a11b8687805ce4611535373bfc9bac
Chris Gonzales <chrisg32@users.noreply.github.com>
2024-07-31 21:09:46 -0500

    Boiler plate

---
+++ src/AvaloniaPdfViewer/AvaloniaPdfViewer/PdfViewer.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaPdfViewer;
+
+public partial class PdfViewer : UserControl
+{
+    public PdfViewer()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/AvaloniaPdfViewer/AvaloniaPdfViewerExample/AvaloniaPdfViewerExample/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace AvaloniaPdfViewerExample.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
